704d70cb3caf1472e6f2068d9027240b138c3fa5
Aayushchy <caayush96@gmail.com>
2023-09-18 09:37:09 +0545

    Fake SMTP Server to test Email

--- src/main/java/com/esewa/usermanagement/configuration/RedisConfiguration.java
+++ src/main/java/com/esewa/usermanagement/configuration/RedisConfiguration.java
@@ -1,10 +1,18 @@
 package com.esewa.usermanagement.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+import java.time.Duration;
+import java.util.Set;
 
 @Configuration
 public class RedisConfiguration {
@@ -21,6 +29,26 @@ public class RedisConfiguration {
         configuration.setHostName(REDIS_HOST);
         configuration.setPort(REDIS_PORT);
         return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    @Primary
+    public CacheManager emailTemplateCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(8));
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
+    @Bean
+    public CacheManager smsTemplateCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5));
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .initialCacheNames(Set.of("smsTemplateCacheManager"))
+                .build();
     }
 
 }
