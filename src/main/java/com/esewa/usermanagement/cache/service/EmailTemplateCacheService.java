ef68b315b02da37d7327cd281e63c252bd23cc99
Aayushchy <31137336+Aayushchy@users.noreply.github.com>
2023-09-18 10:24:39 +0545

    Fake SMTP Server to test Email (#12)
    Implement fate smtp server

--- src/main/java/com/esewa/usermanagement/cache/service/EmailTemplateCacheService.java
+++ src/main/java/com/esewa/usermanagement/cache/service/EmailTemplateCacheService.java
@@ -3,6 +3,7 @@ package com.esewa.usermanagement.cache.service;
 import com.esewa.usermanagement.notification.entity.EmailTemplate;
 import com.esewa.usermanagement.notification.repository.EmailTemplateRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,8 @@ import java.util.Optional;
 public class EmailTemplateCacheService {
 
     private final EmailTemplateRepository emailTemplateRepository;
-    @Cacheable(cacheNames = "registrationEmailTemplate", key = "#templateCode", unless = "#result.isEmpty()")
+
+    @Cacheable(cacheNames = "registrationEmailTemplate", cacheManager = "emailTemplateCacheManager", key = "#templateCode", unless = "#result == null")
     public Optional<EmailTemplate> getTemplateMessage(long templateCode) {
         return emailTemplateRepository.findById(templateCode);
     }
